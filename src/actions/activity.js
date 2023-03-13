d7c6f5af419876b76f4af6fa8a462077f9e6951f
Sayali2811 <sayalidhadge2811@gmail.com>
2023-03-13 12:07:40 +0530

    activity

--- src/actions/activity.js
+++ src/actions/activity.js
@@ -1,14 +1,73 @@
-import {   CLIENT_MSG } from "../constants/actionTypes";
+import { CLIENT_MSG } from "../constants/actionTypes";
 import * as api from "../api";
 
-export const getActivity = (navigate) => async (dispatch) => {
+export const getActivity = () => async (dispatch) => {
     try {
-      const { data, status } = await api.getActivity(formdata);
+      const { data, status } = await api.getActivity();
       dispatch({
         type: CLIENT_MSG,
         message: { info: data.successMessage, status },
       });
       
+    } catch (error) {
+      dispatch({
+        type: CLIENT_MSG,
+        message: {
+          info: error.response.data?.message,
+          status: error.response.status,
+        },
+      });
+      console.log(error);
+    }
+  };
+
+  export const getSubtype = () => async (dispatch) => {
+    try {
+      const { data, status } = await api.getSubtype();
+      dispatch({
+        type: CLIENT_MSG,
+        message: { info: data.successMessage, status },
+      });
+      
+    } catch (error) {
+      dispatch({
+        type: CLIENT_MSG,
+        message: {
+          info: error.response.data?.message,
+          status: error.response.status,
+        },
+      });
+      console.log(error);
+    }
+  };
+
+  export const getCategory = () => async (dispatch) => {
+    try {
+      const { data, status } = await api.getCategory();
+      dispatch({
+        type: CLIENT_MSG,
+        message: { info: data.successMessage, status },
+      });
+      
+    } catch (error) {
+      dispatch({
+        type: CLIENT_MSG,
+        message: {
+          info: error.response.data?.message,
+          status: error.response.status,
+        },
+      });
+      console.log(error);
+    }
+  };
+
+  export const addActivity = (formData) => async (dispatch) => {
+    try {
+      const { data, status } = await api.addActivity(formData);
+      dispatch({
+        type: CLIENT_MSG,
+        message: { info: data.successMessage, status },
+      });
     } catch (error) {
       dispatch({
         type: CLIENT_MSG,
