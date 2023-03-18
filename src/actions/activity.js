4a4bfd8577c4963e263e9c289332959fae99e6ae
Sayali2811 <sayalidhadge2811@gmail.com>
2023-03-18 16:31:44 +0530

    Profile page added

--- src/actions/activity.js
+++ src/actions/activity.js
@@ -1,81 +1,84 @@
-import { CLIENT_MSG } from "../constants/actionTypes";
+import {
+  CLIENT_MSG,
+  ACTIVITY_CATEGORY,
+  ACTIVITY_SUBTYPE,
+  ACTIVITY_TYPE,
+  ACTIVITY_PLACEHOLDER
+} from "../constants/actionTypes";
 import * as api from "../api";
 
 export const getActivity = () => async (dispatch) => {
-    try {
-      const { data, status } = await api.getActivity();
-      dispatch({
-        type: CLIENT_MSG,
-        message: { info: data.successMessage, status },
-      });
-      
-    } catch (error) {
-      dispatch({
-        type: CLIENT_MSG,
-        message: {
-          info: error.response.data?.message,
-          status: error.response.status,
-        },
-      });
-      console.log(error);
-    }
-  };
+  try {
+    const { data, status } = await api.getActivity();
+    dispatch({ type: ACTIVITY_TYPE, payload: data });
+  } catch (error) {
+    dispatch({
+      type: CLIENT_MSG,
+      message: {
+        info: error.response.data?.message,
+        status: error.response.status,
+      },
+    });
+    console.log(error);
+  }
+};
 
-  export const getSubtype = () => async (dispatch) => {
-    try {
-      const { data, status } = await api.getSubtype();
-      dispatch({
-        type: CLIENT_MSG,
-        message: { info: data.successMessage, status },
-      });
-      
-    } catch (error) {
-      dispatch({
-        type: CLIENT_MSG,
-        message: {
-          info: error.response.data?.message,
-          status: error.response.status,
-        },
-      });
-      console.log(error);
-    }
-  };
+export const getSubtype = (type) => async (dispatch) => {
+  try {
+    const { data, status } = await api.getSubtype(type);
+    dispatch({ type: ACTIVITY_SUBTYPE, payload: data });
+  } catch (error) {
+    dispatch({
+      type: CLIENT_MSG,
+      message: {
+        info: error.response.data?.message,
+        status: error.response.status,
+      },
+    });
+    console.log(error);
+  }
+};
 
-  export const getCategory = () => async (dispatch) => {
-    try {
-      const { data, status } = await api.getCategory();
-      dispatch({
-        type: CLIENT_MSG,
-        message: { info: data.successMessage, status },
-      });
-      
-    } catch (error) {
-      dispatch({
-        type: CLIENT_MSG,
-        message: {
-          info: error.response.data?.message,
-          status: error.response.status,
-        },
-      });
-      console.log(error);
-    }
-  };
+export const getCategory = (subtype) => async (dispatch) => {
+  try {
+    const { data, status } = await api.getCategory(subtype);
+    dispatch ({type:ACTIVITY_CATEGORY,payload:data})
+  } catch (error) {
+    dispatch({
+      type: CLIENT_MSG,
+      message: {
+        info: error.response.data?.message,
+        status: error.response.status,
+      },
+    });
+    console.log(error);
+  }
+};
 
-  export const addActivity = (formData) => async (dispatch) => {
-    try {
-      const { data, status } = await api.addActivity(formData);
-      dispatch({
-        type: CLIENT_MSG,
-        message: { info: data.successMessage, status },
-      });
-    } catch (error) {
-      dispatch({
-        type: CLIENT_MSG,
-        message: {
-          info: error.response.data?.message,
-          status: error.response.status,
-        },
-      });
-      console.log(error);
-    }
-  };
+export const getPlaceHolder = (category) => async (dispatch) => {
+  try {
+    const { data, status } = await api.getPlaceHolder(category);
+    dispatch ({type:ACTIVITY_PLACEHOLDER,payload:data.placeholder})
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const addActivity = (formData) => async (dispatch) => {
+  try {
+    const { data, status } = await api.addActivity(formData);
+    dispatch({
+      type: CLIENT_MSG,
+      message: { info: data.successMessage, status },
+    });
+  } catch (error) {
+    dispatch({
+      type: CLIENT_MSG,
+      message: {
+        info: error.response.data?.message,
+        status: error.response.status,
+      },
+    });
+    console.log(error);
+  }
+};
