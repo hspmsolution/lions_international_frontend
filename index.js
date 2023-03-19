09ca15ea03d925dd16a33b411358b6bc110bfae1
Sanket <sanketanilkadam@gmail.com>
2023-03-19 19:24:08 +0530

    docker setup

--- index.js
+++ index.js
@@ -1,23 +1,15 @@
-import express from "express";
+const express = require('express');
+const path = require('path');
 const app = express();
-import * as dotenv from "dotenv";
-dotenv.config();
-import path from "path";
-import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// Serve the static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.use(express.static(path.join(__dirname, "client", "build")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// Handles any requests that don't match the above
+app.get('*', (req,res) =>{
+  res.sendFile(path.join(__dirname+'/build/index.html'));
 });
 
 const PORT = process.env.PORT || 5000;
 
-
-
-app.listen(PORT, () =>
-  console.log(`Server Running on Port: http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`))
