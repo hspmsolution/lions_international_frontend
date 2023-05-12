2fe36cb036b094b2a8067e9dd3a19199c45868f2
Sunil Aadewar <sunilaadewar@gmail.com>
2023-05-12 14:02:01 +0530

    Dynamic Pre-Loader01

--- src/App.js
+++ src/App.js
@@ -23,19 +23,22 @@ export default function App() {
 
   const [isLoaded, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
-
+  // Static Pre-Loader
   // useEffect(() => {
-  //   window.onload = () => {
-  //     console.log("Page Has Loaded");
-  //      setIsLoading(false);
-  //   };
+  //   setTimeout(() => {
+  //     setIsLoading(false);
+  //   }, 3000);
   // }, []);
 
+  // Dynamin Pre-Loader01
+  useEffect(() => {
+    window.onload = () => {
+      console.log("Page Has Loaded");
+      setIsLoading(false);
+    };
+  }, []);
+
+  // Dynamin Pre-Loader01
   // window.addEventListener("load", () => {
   //   if (document.readyState === "complete") {
   //     // window.onload has completed
--- src/components/CommonCard/CardStyle.js
+++ src/components/CommonCard/CardStyle.js
@@ -1,23 +1,22 @@
-import { makeStyles } from '@mui/styles';
+import { makeStyles } from "@mui/styles";
 
 export default makeStyles((theme) => ({
-   
-    activityImage:{
-       '@media screen and (max-width: 600px)' :{
-        height:'250px'
-        },
-        width:'100%',
-        height:'300px'
+  activityImage: {
+    "@media screen and (max-width: 600px)": {
+      height: "250px",
     },
-    
-    activityDate:{
-        position:'absolute',
-        top:'6%',
-        right:'4%',
-        backgroundColor:'white',
-        padding:'0.2rem 0.6rem',
-        borderRadius:'1rem',
-        color:'red',
-    }
+    width: "100%",
+    height: "300px",
+  },
 
-}))
+  activityDate: {
+    position: "absolute",
+    top: "6%",
+    right: "4%",
+    backgroundColor: "white",
+    padding: "0.2rem 0.6rem",
+    borderRadius: "1rem",
+    color: "red",
+    margin: "0",
+  },
+}));
