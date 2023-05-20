a996884206cb15838a3cdb507fb68ebfb0db9eb8
Rohit <rohitmore386@gmail.com>
2023-05-20 11:43:30 +0530

    correcting change

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -16,7 +16,7 @@ function About() {
                     <h1 className="about-dist-h">About District 317F</h1>
                     <p>
                         Welcome to our website of lions club international’s district 317F.
-                        and all members of our district, #317F. sow service seeds with
+                        and all members of our district, 317F. sow service seeds with
                         kindness in unity and diversity to give more for community. your journey
                         through the web pages is made extremely simple with distinct information
                         about our organization and its administrative functioning and reporting.
